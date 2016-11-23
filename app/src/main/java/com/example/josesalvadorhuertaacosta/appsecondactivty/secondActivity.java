5f2481a15107e03b045d4c297c00235e061019e9
Jos Huerta <jsalvador@websinergia.com.mx>
2016-11-23 15:49:31 -0600

    se terminal list view con click por elemento y el portrait que es vertical

--- app/src/main/java/com/example/josesalvadorhuertaacosta/appsecondactivty/secondActivity.java
+++ app/src/main/java/com/example/josesalvadorhuertaacosta/appsecondactivty/secondActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -22,6 +25,18 @@ public class secondActivity extends AppCompatActivity implements View.OnClickLis
         texto = (TextView) findViewById(R.id.text);
 
         listView  = (ListView) findViewById(R.id.lista);
+        String[] valores = new String[]{"Holanda", "España", "EU", "India", "Mexico", "China", "Uruguay", "Brasil"};
+
+        //se crea adaptador para llenar un elemento como queremos
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, valores);
+        listView.setAdapter(adapter);
+        // para poder dar clck a cada elemento de la lista
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Position"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -31,7 +46,7 @@ public class secondActivity extends AppCompatActivity implements View.OnClickLis
             texto.setText(dato);
         }
 
-        int[] numeros = new int[4];
+        /*int[] numeros = new int[4];
         numeros[0] = 1;
         numeros[1] = 2;
         numeros[2] = 3;
@@ -40,7 +55,9 @@ public class secondActivity extends AppCompatActivity implements View.OnClickLis
         for (int i=1; i<numeros.length; i++){
 
             System.out.println(i+"prueba de escritura  ----------------------------------------");
-        }
+        }*/
+
+
     }
 
     @Override
